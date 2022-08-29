4316e546c05af7adfc7b035e2f8310144470c5d8
aslancan.yilmaz <aslancanyilmaz@tiklagelsin.com>
2022-08-30 01:56:49 +0300

    Added CustomImage component

---
+++ src/components/CustomImage/image.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+import {ActivityIndicator, View} from 'react-native';
+import FastImage from 'react-native-fast-image';
+import {COLORS, SIZES} from '../../constants/theme';
+import styles from './styles';
+
+const CustomImage = props => {
+  const {image, marginSize = 0, containerStyle, customWidth} = props;
+  const [loading, setLoading] = useState(true);
+  const imageWidth = (customWidth ?? SIZES.screenWidth) - marginSize;
+  const ratio = 1.4;
+  const defaultImageStyle = {
+    width: imageWidth,
+    height: imageWidth / ratio,
+  };
+
+  return (
+    <View
+      style={{...containerStyle, ...styles.container, ...defaultImageStyle}}>
+      {loading ? <ActivityIndicator color={COLORS.black} /> : null}
+      <FastImage
+        onLoadStart={() => setLoading(true)}
+        onLoadEnd={() => setLoading(false)}
+        source={{uri: image}}
+        style={loading ? styles.imageStyleOnLoading : defaultImageStyle}
+        resizeMode={FastImage.resizeMode.cover}
+        {...props}>
+        {props.children}
+      </FastImage>
+    </View>
+  );
+};
+
+export {CustomImage};
