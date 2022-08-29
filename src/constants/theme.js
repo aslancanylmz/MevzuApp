f7e4b53a426bc83cc74817fcc483e5c1711707f8
aslancan.yilmaz <aslancanyilmaz@tiklagelsin.com>
2022-08-29 14:02:04 +0300

    Add theme constants and icon component

---
+++ src/constants/theme.js
@@ -0,0 +1,10 @@
+import {Dimensions} from 'react-native';
+export const COLORS = {
+  white: '#fff',
+  black: '#000',
+};
+
+export const SIZES = {
+  screenWidth: Dimensions.get('screen').width,
+  screenHeight: Dimensions.get('screen').height,
+};
