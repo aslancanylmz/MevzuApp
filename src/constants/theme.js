34c750bd917925f0a24c2ee271c5ad60f0c30c0f
aslancan.yilmaz <aslancanyilmaz@tiklagelsin.com>
2022-09-01 03:42:19 +0300

    Added shadow styles

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -2,9 +2,25 @@ import {Dimensions} from 'react-native';
 export const COLORS = {
   white: '#fff',
   black: '#000',
+  green: '#39e75b',
+  shadow: '#999',
+  red:'#ff4045'
 };
 
 export const SIZES = {
   screenWidth: Dimensions.get('screen').width,
   screenHeight: Dimensions.get('screen').height,
+};
+
+export const STYLES = {
+  dropShadow: {
+    shadowColor: COLORS.shadow,
+    shadowOffset: {
+      width: 1,
+      height: 2,
+    },
+    shadowOpacity: 0.4,
+    shadowRadius: 0.7,
+    elevation: 3,
+  },
 };
